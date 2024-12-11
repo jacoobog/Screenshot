908bdae8bf06542504b80e8734ba9dee34be02db
Jacoo <3024514165@qq.com>
2024-12-12 01:39:46 +0800

    添加cmake环境，未完成

---
+++ build/CMakeFiles/release.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "J:/programming/c/Screenshot/Screenshot/CatchScreenDlg.cpp" "CMakeFiles/release.dir/Screenshot/CatchScreenDlg.obj" "gcc" "CMakeFiles/release.dir/Screenshot/CatchScreenDlg.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/MyEdit.cpp" "CMakeFiles/release.dir/Screenshot/MyEdit.obj" "gcc" "CMakeFiles/release.dir/Screenshot/MyEdit.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/MyToolBar.cpp" "CMakeFiles/release.dir/Screenshot/MyToolBar.obj" "gcc" "CMakeFiles/release.dir/Screenshot/MyToolBar.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/MyTracker.cpp" "CMakeFiles/release.dir/Screenshot/MyTracker.obj" "gcc" "CMakeFiles/release.dir/Screenshot/MyTracker.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/PopUpMenu.cpp" "CMakeFiles/release.dir/Screenshot/PopUpMenu.obj" "gcc" "CMakeFiles/release.dir/Screenshot/PopUpMenu.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/Screenshot.cpp" "CMakeFiles/release.dir/Screenshot/Screenshot.obj" "gcc" "CMakeFiles/release.dir/Screenshot/Screenshot.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/ScreenshotDlg.cpp" "CMakeFiles/release.dir/Screenshot/ScreenshotDlg.obj" "gcc" "CMakeFiles/release.dir/Screenshot/ScreenshotDlg.obj.d"
+  "J:/programming/c/Screenshot/Screenshot/stdafx.cpp" "CMakeFiles/release.dir/Screenshot/stdafx.obj" "gcc" "CMakeFiles/release.dir/Screenshot/stdafx.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/release.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/release.dir/Screenshot/CatchScreenDlg.obj"
+  "CMakeFiles/release.dir/Screenshot/CatchScreenDlg.obj.d"
+  "CMakeFiles/release.dir/Screenshot/MyEdit.obj"
+  "CMakeFiles/release.dir/Screenshot/MyEdit.obj.d"
+  "CMakeFiles/release.dir/Screenshot/MyToolBar.obj"
+  "CMakeFiles/release.dir/Screenshot/MyToolBar.obj.d"
+  "CMakeFiles/release.dir/Screenshot/MyTracker.obj"
+  "CMakeFiles/release.dir/Screenshot/MyTracker.obj.d"
+  "CMakeFiles/release.dir/Screenshot/PopUpMenu.obj"
+  "CMakeFiles/release.dir/Screenshot/PopUpMenu.obj.d"
+  "CMakeFiles/release.dir/Screenshot/Screenshot.obj"
+  "CMakeFiles/release.dir/Screenshot/Screenshot.obj.d"
+  "CMakeFiles/release.dir/Screenshot/ScreenshotDlg.obj"
+  "CMakeFiles/release.dir/Screenshot/ScreenshotDlg.obj.d"
+  "CMakeFiles/release.dir/Screenshot/stdafx.obj"
+  "CMakeFiles/release.dir/Screenshot/stdafx.obj.d"
+  "librelease.dll.a"
+  "release.exe"
+  "release.exe.manifest"
+  "release.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/release.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
